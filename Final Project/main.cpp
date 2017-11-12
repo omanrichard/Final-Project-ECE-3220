84fc806f2a441fb0c9b80e4f47514461e43f32f4
Richard A. Oman <richard.oman@me.com>
2017-11-11 18:16:10 -0600

    Initial Commit

---
+++ Final Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Final Project
+//
+//  Created by Richard Oman on 11/11/17.
+//  Copyright © 2017 Richard Oman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
